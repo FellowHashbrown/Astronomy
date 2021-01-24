fe2bfa7abee0e78ed348a27f306cfb9920f8dabb
Jonah Pierce <fellowhashbrown@gmail.com>
2021-01-24 07:11:56 +0000

    Setup the yearly sections and monthly subsections in the main page

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
 from flask import Flask, render_template, redirect
 from random import randint
 from threading import Thread
+from os import environ
+
+from requests import get
 
 # # # # # # # # # # # # # # # # # # # # 
 
 app = Flask("Fellow Hashbrown APIs")
+imgur_hashes = {
+    "2021": {
+        "January": "GZgoSdg"
+    }
+}
+imgur_images = {}
 
 # # # # # # # # # # # # # # # # # # # # 
 
 @app.route("/")
 def home():
-    pass
+    
+    # Go through each of the months in each year
+    #   to move the image links into the dictionary
+    for year in imgur_hashes:
+
+        # If the year does not exist in the images yet, create it
+        if year not in imgur_images:
+            imgur_images[year] = {}
+        
+        # Iterate through the months in the hashes
+        for month in imgur_hashes[year]:
+
+            # If the month does not exist in the images yet, create it
+            if month not in imgur_images[year]:
+                imgur_images[year][month] = []
+            
+            # Load the imgur album's images, if there are any
+            if imgur_hashes[year][month] is not None:
+                images = get(
+                    "https://api.imgur.com/3/album/{}/images".format(
+                        imgur_hashes[year][month]),
+                    headers = {
+                        "Authorization": "Client-ID {}".format(environ["IMGUR_API_KEY"])
+                    }
+                ).json()["data"]
+                for i in range(len(images)):
+                    images[i] = {
+                        "link": images[i]["link"],
+                        "description": images[i]["description"]
+                    }
+                imgur_images[year][month] = images
+            else:
+                imgur_images[year][month] = None
+    
+    return render_template(
+        "astronomy.html",
+        years = imgur_images
+    )
 
 @app.route("/favicon")
 @app.route("/favicon.ico")
@@ -22,7 +68,7 @@ def favicon():
 def run():
     app.run(
         host = '0.0.0.0', 
-        port = randint(1000, 9999))
+        port = randint(1025, 9999))
 
 t = Thread(target = run)
 t.start()
